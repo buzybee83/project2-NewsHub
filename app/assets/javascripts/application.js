3f98bb6fc745c2d80b1e12b2290255a5a6d029dc
Guivia Moreira <crea7ive.d@gmail.com>
2015-09-18 18:05:04 -0400

    Added background image & masonry view configured

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,3 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_and_belongs_to_many :feeds
+
+  def user_name
+     "#{first_name} #{last_name}"
+  end
+
+  def to_s
+    user_name
+  end
 end
